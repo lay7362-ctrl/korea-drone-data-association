76a69e9a9601b7cbbdb19e7f14b708eabd1f2327
lay7362 <lay7362@gmail.com>
2026-06-19 16:55:14 +0900

    푸터에 실제 협회 주소 추가 및 이메일을 kdda.office1@gmail.com으로 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ function Footer() {
     <footer className="site-footer">
       <div className="site-footer__inner">
         <p className="site-footer__name">한국드론앤데이터협회</p>
-        <p>문의: info@kdda.or.kr · 전화: 044-864-0694</p>
+        <p>세종시 갈매로 353 세종에비뉴힐 A동 5018호</p>
+        <p>문의: kdda.office1@gmail.com · 전화: 044-864-0694</p>
         <p className="site-footer__copy">
           © {new Date().getFullYear()} Korea Drone &amp; Data Association. All
           rights reserved.
