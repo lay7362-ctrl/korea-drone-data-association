9f0bff833adb7782d5ca8b3892c7db08f3d8b394
lay7362 <lay7362@gmail.com>
2026-06-19 16:51:07 +0900

    푸터 전화번호 044-864-0694로 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="site-footer">
       <div className="site-footer__inner">
         <p className="site-footer__name">한국드론앤데이터협회</p>
-        <p>문의: info@kdda.or.kr · 전화: 02-0000-0000</p>
+        <p>문의: info@kdda.or.kr · 전화: 044-864-0694</p>
         <p className="site-footer__copy">
           © {new Date().getFullYear()} Korea Drone &amp; Data Association. All
           rights reserved.
